2c78af8242dcbb8e9771a2ba864e9a8f1c0d762d
SpacyFranky <bigaziz@outlook.com>
2019-12-12 18:11:43 +0000

    who am i?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    print(hidden_4.my_secret_santa.__name__)
+    print(hidden_4.print_holberton.__name__)
+    print(hidden_4.print_school.__name__)
