fb1e4a62d32500bdef5539b1946c1b2682d050b8
SpacyFranky <bigaziz@outlook.com>
2019-12-17 18:18:14 +0000

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return(None)
     length = len(sentence)
+    if sentence == "":
+        return(length, None)
     first = sentence[0:1]
     tuple_s = (length, first)
     return(tuple_s)
