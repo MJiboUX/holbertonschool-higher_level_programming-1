ba4d515a327f6361bb155a9e6c109b2e92ec8ee3
SpacyFranky <bigaziz@outlook.com>
2019-12-19 11:07:40 +0000

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     s = 0
-    # creating an empty hashet
-    hashet = dict()
+    hashet = dict()  # creating an empty hashet.
     for i in range(len(my_list)):
-        #if it's not in the hashet, it'll be added
-        if (my_list[i] not in hashet.keys()):
-            hashet[my_list[i]] = my_list[i]
+        if (my_list[i] not in hashet.keys()):  # if it's not in the hashet,
+            hashet[my_list[i]] = my_list[i]    # it'll be added.
             s += my_list[i]
     return(s)
