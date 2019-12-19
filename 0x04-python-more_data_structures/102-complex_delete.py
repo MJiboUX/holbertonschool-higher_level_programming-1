affdf8ff7ee3d84f9376bb28f791ad5bda07ba58
SpacyFranky <bigaziz@outlook.com>
2019-12-19 19:01:40 +0000

    Delete by value (worked after trying many methods)

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,7 +4,18 @@ def complex_delete(a_dictionary, value):
     #    del(a_dictionary[value])
     # return(a_dictionary)
 
-    for k, v in enumerate(a_dictionary):
+    # for k, v in enumerate(a_dictionary):
+    #    if v == value:
+    #        del(a_dictionary[k])
+    # return(a_dictionary)
+
+    # a = {k:v for k, v in a_dictionary.items() if v != value}
+    # return(a)
+
+    pop_list = []
+    for k, v in a_dictionary.items():
         if v == value:
-            del(a_dictionary[k])
+            pop_list.append(k)
+    for x in pop_list:
+        a_dictionary.pop(x)
     return(a_dictionary)
