8c29c3868b5fe8dbadea1f0e06172b13272d9149
SpacyFranky <bigaziz@outlook.com>
2020-01-08 18:39:30 +0000

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    try:
+        q = a / b
+        return(a / b)
+    except ZeroDivisionError:
+        q = None
+        return(None)
+    finally:
+        print("Inside result: {}".format(q))
