2bfc00fcebb35b83dbf44878f31ac3ea5965245c
SpacyFranky <bigaziz@outlook.com>
2020-01-08 16:39:59 +0000

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        a = 0
+        for i in my_list:
+            if a < x:
+                try:
+                    print("{:d}".format(i), end="")
+                    a += 1
+                except:
+                    pass
+        print()
+        return(a)
+    except:
+        a = 0
+        for i in my_list:
+            try:
+                print("{:d}".format(i), end="")
+                a += 1
+            except:
+                pass
+        print()
+        return(a-1)
