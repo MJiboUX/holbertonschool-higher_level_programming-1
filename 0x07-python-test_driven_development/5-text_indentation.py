79de98f81e75cfccd10c7b0a3b32a4d988f852ef
SpacyFranky <bigaziz@outlook.com>
2020-01-14 17:18:13 +0000

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+"""
+"""
+def text_indentation(text):
+    """
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    print(text.split('.'))
+    # with this split function It'll make it easy to print right text.
