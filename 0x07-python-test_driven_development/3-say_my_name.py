adf1f33596afe76c3a11465cf82e40e6c49f973d
SpacyFranky <bigaziz@outlook.com>
2020-01-15 12:26:47 +0000

    added if function is empty statement

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,6 +14,8 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
+    if type(first_name) is None and type(last_name) is None:
+        raise TypeError("first_name must be a string")
     if last_name is None:
         print("My name is {} ".format(first_name))
     print("My name is {} {}".format(first_name, last_name))
