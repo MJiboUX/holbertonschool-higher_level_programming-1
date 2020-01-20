69edfb606c6351853d85a48bd242a217b213a7f7
SpacyFranky <bigaziz@outlook.com>
2020-01-20 16:37:31 +0000

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,24 @@
 """
+text indentation function
 """
+
+
 def text_indentation(text):
-    """
+    """prints a text with 2 new lines after each of ".", "?" and ":"
+       Args:
+           text: the text that will be printed with the new lines.
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    print(text.split('.'))
-    # with this split function It'll make it easy to print right text.
+
+    s = list(text)
+    for i in range(len(s)):
+        if s[i] == ".":
+            s[i+1] = "\n\n"
+        if s[i] == ":":
+            s[i+1] = "\n\n"
+        if s[i] == "?":
+            s[i+1] = "\n\n"
+    nospace = ""
+    nospace = nospace.join(s)
+    print(nospace)
