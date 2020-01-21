993e18478dbbceb4a48e755f1bbccb112ccdb995
SpacyFranky <bigaziz@outlook.com>
2020-01-21 09:49:23 +0000

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Inheritance
+"""
+def lookup(obj):
+    """function that returns the list of available attributes
+       and methods of an object.
+       Args:
+           obj: an object
+       Returns:
+           a list object
+    """
+    return(dir(obj))
