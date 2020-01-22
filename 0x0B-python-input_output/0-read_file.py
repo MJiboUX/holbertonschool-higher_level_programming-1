dded14daa820cf9d2e95c6783fd7163eed953236
SpacyFranky <bigaziz@outlook.com>
2020-01-22 08:44:05 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Reading a file
+"""
+def read_file(filename=""):
+    """function that read a file
+       Args:
+           filename (str): the file
+    """
+    with open(filename, 'r') as f:
+        read_file = f.read()
+        print(read_file, end='')
