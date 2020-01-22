9138953f469c888cfb9661c41b39daf6d3712c8e
SpacyFranky <bigaziz@outlook.com>
2020-01-22 17:11:25 +0000

    Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class to JSON
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+       (list, dictionary, string, integer and boolean) for JSON
+       serialization of an object
+       Args:
+           obj (str): an object
+       Return: the dictionary description with simple data structure
+    """
+    return(obj.__dict__)
