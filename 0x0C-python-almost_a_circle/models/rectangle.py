a6101764cf62de680c9c2bab37952b2590cffc55
SpacyFranky <bigaziz@outlook.com>
2020-01-27 23:08:16 +0000

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,6 +40,13 @@ class Rectangle(Base):
         if id is not None:
             self.id = id
 
+    def __str__(self):
+        """Returns:
+               [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        return("[Rectangle] ({}) {}/{} - {}/{}"
+               .format(self.id, self.x, self.y, self.width, self.height))
+
     @property
     def width(self):
         """width getter
