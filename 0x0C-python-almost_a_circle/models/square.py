96ee6707d572246efa96b681b991123b89ff0ad8
SpacyFranky <bigaziz@outlook.com>
2020-01-28 16:46:59 +0000

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+And now, the Square!
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor, creates a new instance
+           of a class
+           Args:
+               size (int): size of square
+               x (int): x-axis
+               y (int): y-axis
+               id (int): id
+        """
+        super().__init__(size, size, x, y, id)
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        """Returns:
+               [Square] (<id>) <x>/<y> - <size>
+        """
+        return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width))
