04c7421bbe86f09330a638cb3772e7de34ec447e
SpacyFranky <bigaziz@outlook.com>
2020-03-03 22:18:58 +0000

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- A script that lists all the cities of California that can be found
+-- in the database hbtn_0d_usa
+SELECT * FROM cities
+WHERE cities.name = "California"
+ORDER BY cities.id ASC;
