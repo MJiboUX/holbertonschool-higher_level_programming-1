db21c517658c40ce8f45208edb9a774c94e7a4f3
SpacyFranky <bigaziz@outlook.com>
2020-03-29 18:08:19 +0000

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    d = session.query(State).filter(State.name.like('%a'))
+    d = session.query(State).filter(State.name.like('%a%'))
     d.delete(synchronize_session=False)
     session.commit()
     session.close()
