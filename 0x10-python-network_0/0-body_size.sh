75e918d2bff1510304ef9db239f3cd956023fc63
SpacyFranky <bigaziz@outlook.com>
2020-04-12 15:27:18 +0000

    fixed shellcheck error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This Bash script displays the size of the body of the response
-curl -s $1 | wc -c
+curl -s "$1" | wc -c
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X ALLOW $1 | sed -n 's/^Allow: //p'
+curl -sI -X ALLOW "$1" | sed -n 's/^Allow: //p'
