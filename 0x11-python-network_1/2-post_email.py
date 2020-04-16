71cef8c4a4e5fc200cd8f24c09688fbd4180a410
SpacyFranky <bigaziz@outlook.com>
2020-04-16 10:52:17 +0000

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        print(response.read())
+        print(response.read().decode('utf-8'))
