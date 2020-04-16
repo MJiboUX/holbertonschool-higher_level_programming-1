3d58d47591f1883e2893c801fa3ca90282b69302
SpacyFranky <bigaziz@outlook.com>
2020-04-16 11:41:38 +0000

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,14 @@
 """ Python script that takes in a URL, sends a request to the URL and
     displays the body of the response (decoded in utf-8).
 """
+import sys
 import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         try:
-            print(urllib.request.urlopen(response))
+            print(response.read().decode('utf-8'))
         except urllib.error.HTTPError as e:
             print("Error code: {}".format(e.code))
