eb881ba17cc5824f76de0afa889493c3ca6e8e25
SpacyFranky <bigaziz@outlook.com>
2020-04-16 12:38:36 +0000

    POST an email by using requests package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1], data = {'email':sys.argv[2]})
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r)
