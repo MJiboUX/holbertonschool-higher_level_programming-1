c4e6981bc1d1aa30791f0dad27ae19f1f1a8a307
spacyfranky <bigaziz@outlook.com>
2020-05-05 07:43:20 +0000

    changed method on how to validate int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const process = require('process');
 
-if (Number(process.argv[2])) {
-  console.log('My number: ' + ~~Number(process.argv[2]));
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
