7122f7cc64df85aac9a1c393d02d2e3e1b278820
spacyfranky <bigaziz@outlook.com>
2020-05-05 12:04:32 +0000

    fixing a bug in the code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,16 +5,16 @@ const process = require('process');
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let max = parseInt(process.argv[2]);
+  let max = process.argv[2];
   for (let i = 3; i < process.argv.length; i++) {
-    if (max < parseInt(process.argv[i])) {
-      max = parseInt(process.argv[i]);
+    if (max < process.argv[i]) {
+      max = process.argv[i];
     }
   }
-  let second = parseInt(process.argv[2]);
+  let second = process.argv[2];
   for (let i = 3; i < process.argv.length; i++) {
-    if (second < parseInt(process.argv[i]) && parseInt(process.argv[i]) !== max) {
-      second = parseInt(process.argv[i]);
+    if (second < process.argv[i] && process.argv[i] !== max) {
+      second = process.argv[i];
     }
   }
   console.log(second);
