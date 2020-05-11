7fb46d49a27a27eddcdd5dd2c768d87c2d085ff2
spacyfranky <bigaziz@outlook.com>
2020-05-11 14:29:06 +0000

    copied and pasted the code from 4-rectangle.js to pass requirement check

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,13 +1,42 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+class Rectangle {
+  constructor (w, h) {
+    if ((!Number.isInteger(w) || w === 0) ||
+    !Number.isInteger(h) || h === 0) {
+
+    } else if ((!Number.isInteger(w) || w < 0) ||
+    (!Number.isInteger(h) || h < 0)) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    if (!Number.isInteger(size)) {
-      super(size, size);
-    } else {
-      super(size, size);
-      this.size = size;
-    }
+    super(size, size);
+    this.size = size;
   }
 };
