4eb23fb68f023ba67b17ab58f8023680f64a14d7
spacyfranky <bigaziz@outlook.com>
2020-05-11 13:56:07 +0000

    Rectangle #4 - added rotate() and double() funcitons

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((!Number.isInteger(w) || w === 0) ||
+    !Number.isInteger(h) || h === 0) {
+
+    } else if ((!Number.isInteger(w) || w < 0) ||
+    (!Number.isInteger(h) || h < 0)) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
