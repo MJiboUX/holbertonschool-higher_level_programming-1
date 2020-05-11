90ee1d9ef12cf094129fc8bbc5a6b46e3b710f71
spacyfranky <bigaziz@outlook.com>
2020-05-11 15:02:54 +0000

    Square #1 - fixing requirement errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,12 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined || c === null) {
-      this.print();
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        process.stdout.write('\n');
+      }
     } else {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
