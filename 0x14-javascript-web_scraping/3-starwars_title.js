49dbefc1d4f7cf63e5a4c571edf0560b67c4da85
spacyfranky <bigaziz@outlook.com>
2020-05-12 17:14:13 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-
-request(url, function (err, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
   }
+  console.log(JSON.parse(body).title);
 });
