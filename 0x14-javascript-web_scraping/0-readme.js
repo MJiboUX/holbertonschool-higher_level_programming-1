580dd7d340de2c6d1b95555872f4165a245ca489
spacyfranky <bigaziz@outlook.com>
2020-05-12 15:51:30 +0000

    reading from a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const process = require('process');
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
   console.log(data);
 });
