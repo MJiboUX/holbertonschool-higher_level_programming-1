1f3cb24631858e781d0678d739760c2a1fceb474
spacyfranky <bigaziz@outlook.com>
2020-05-12 15:38:59 +0000

    fixed typo in utf-8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, cont) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
-  console.log(cont);
+  console.log(data);
 });
