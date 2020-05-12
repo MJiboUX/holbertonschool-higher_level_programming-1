2abd920c42e747f48388e90a7b5068089caffa75
spacyfranky <bigaziz@outlook.com>
2020-05-12 16:08:29 +0000

    adjusting error output

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
   function (err, response, body) {
-    if (err) {
+    if (err || !parseInt(process.argv[2])) {
       return console.log(err);
     }
     console.log(JSON.parse(body).title);
